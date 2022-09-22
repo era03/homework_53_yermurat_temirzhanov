5417acea4a4724a8c06519e44dd0489d6fb3ddba
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-09-23 05:41:29 +0600

    Добавленно ноое поле в таблику todo

--- to_do_app/webapp/models.py
+++ to_do_app/webapp/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class ToDo(models.Model):
-    description = models.CharField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
     status = models.TextField(max_length=200, null=False, blank=False, default='new', verbose_name='Статус')
     deadline = models.DateField(verbose_name='Дата выполнения', null=True, default=None)
+    full_description = models.TextField(max_length=200, null=True, blank=False, verbose_name='Подробное описание')
 
 
     def __str__(self) -> str:
